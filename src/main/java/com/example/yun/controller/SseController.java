f840a0e6f32cd7e74d183c7aa317bf009eefe77e
leedoyun <idoyun781@gmail.com>
2025-04-24 20:17:48 +0900

    feat : 기본설정 euc-kr 에서 urf-8로 변경 / 포트 9090으로 변경

--- src/main/java/com/example/yun/controller/SseController.java
+++ src/main/java/com/example/yun/controller/SseController.java
@@ -1,4 +1,4 @@
-package com.example.yun.controller;
+﻿package com.example.yun.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
